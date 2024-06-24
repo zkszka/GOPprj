2844b5159f52fad6482d50dcd0a16209fe0d69ce
zkszka <tenandten10@naver.com>
2024-06-24 17:47:31 +0900

    보호소 api구현했습니다.

--- src/Components/Abandoned/Shelter.js
+++ src/Components/Abandoned/Shelter.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Navbar/Footer';
 
-const ShelterSearch = () => {
+const Shelter = () => {
+  // 검색 조건을 담는 상태 변수들
   const [searchParams, setSearchParams] = useState({
     serviceKey: 'rVXpgEeih%2BlnN57euuJZoWNcJUK2tMLSqy9Nd%2F7cpLHQzJ8to5pNWWTZSHh8luaU5f%2Blxc2zmZzMYu4gytBTrg%3D%3D',
     care_nm: '',
@@ -11,10 +12,17 @@ const ShelterSearch = () => {
     pageNo: '1',
     _type: 'json'
   });
+
+  // API 호출 중 상태 관리
   const [loading, setLoading] = useState(false);
+
+  // API 응답 데이터 관리
   const [data, setData] = useState(null);
+
+  // API 호출 중 발생한 오류 관리
   const [error, setError] = useState(null);
 
+  // 입력 필드 값 변경 시 호출되는 함수
   const handleChange = (e) => {
     const { name, value } = e.target;
     setSearchParams({
@@ -23,25 +31,32 @@ const ShelterSearch = () => {
     });
   };
 
+  // 폼 제출 시 호출되는 함수
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
     try {
+      // 쿼리 파라미터 생성
       const queryParams = `?${Object.keys(searchParams).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(searchParams[key])}`).join('&')}`;
-      const url = 'http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic';
+      // API 엔드포인트 URL
+      const url = 'http://apis.data.go.kr/1543061/animalShelterSrvc/shelterInfo';
+      // API 호출
       const response = await axios.get(url + queryParams);
+      // 데이터 설정
       setData(response.data);
     } catch (error) {
+      // 오류 발생 시 오류 메시지 설정
       console.error('API 호출 에러:', error);
       setError('데이터를 가져오는 중 오류가 발생했습니다.');
     } finally {
+      // 로딩 상태 해제
       setLoading(false);
     }
   };
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <h1>동물보호센터 검색</h1>
       <form onSubmit={handleSubmit}>
         <label>동물보호센터명:</label>
@@ -56,8 +71,11 @@ const ShelterSearch = () => {
         <button type="submit">검색</button>
       </form>
 
+      {/* 로딩 중일 때 표시 */}
       {loading && <p>Loading...</p>}
+      {/* 오류 발생 시 표시 */}
       {error && <p>{error}</p>}
+      {/* 데이터가 있는 경우 표시 */}
       {data && data.response && data.response.body && data.response.body.items && data.response.body.items.item ? (
         <div>
           <h2>검색 결과</h2>
@@ -131,9 +149,9 @@ const ShelterSearch = () => {
       ) : (
         <p>No data found.</p>
       )}
-      <Footer/>
+      <Footer />
     </div>
   );
 };
 
-export default ShelterSearch;
+export default Shelter;
